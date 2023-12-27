2c08c27d261d6f761cd9088c1cf86715fec318fa
Akmaljon571 <akmaljon079@gmail.com>
2023-12-27 17:23:27 +0300

    fix: order count add and depletion add rout

--- order/urls.py
+++ order/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', OrderCreate.as_view()),
     path('user/<int:user_id>', OrderAPIView.as_view()),
-    path('count/<int:order_id>', OrderCount.as_view()),
+    path('add/<int:order_id>', OrderAddCount.as_view()),
+    path('depletion/<int:order_id>', OrderDepletionCount.as_view()),
     path('user/<int:user_id>/pro/<int:pro_id>', OrderDelete.as_view()),
 ]
--- order/views.py
+++ order/views.py
@@ -12,6 +12,10 @@ class OrderAPIView(APIView):
         orders = OrderModel.objects.filter(user_id=user_id)
         serializer = OrderSerializer(orders, many=True)
         jami = 0
+
+        for a in serializer.data:
+            jami += int(a['product']['price']) * int(a["count"])
+
         return Response({"data": serializer.data, "jami": jami})
 
 
@@ -24,7 +28,7 @@ class OrderCreate(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class OrderCount(APIView):
+class OrderAddCount(APIView):
     def put(self, request, *args, **kwargs):
         order_id = kwargs.get('order_id')
         try:
@@ -32,21 +36,28 @@ class OrderCount(APIView):
         except OrderModel.DoesNotExist:
             return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        action = request.data.get('action')
-
-        if action == 'increment':
-            order.count += 1
-        elif action == 'decrement':
-            if order.count > 0:
-                order.count -= 1
-            else:
-                return Response({'error': 'Count cannot be negative'}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response({'error': 'Invalid action'}, status=status.HTTP_400_BAD_REQUEST)
+        order.count += 1
 
         order.save()
         serializer = OrderSerializer(order)
         return Response(serializer.data)
+
+
+class OrderDepletionCount(APIView):
+    def put(self, request, *args, **kwargs):
+        order_id = kwargs.get('order_id')
+        try:
+            order = OrderModel.objects.get(id=order_id)
+        except OrderModel.DoesNotExist:
+            return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        if order.count > 1:
+            order.count -= 1
+
+            order.save()
+            serializer = OrderSerializer(order)
+            return Response(serializer.data)
+        return Response({'error': 'Order count == 1'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class OrderDelete(APIView):
