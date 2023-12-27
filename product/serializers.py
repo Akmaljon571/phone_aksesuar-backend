e931644e97a43cb7db9be38b7ca9ca6b6385ae74
Akmaljon571 <akmaljon079@gmail.com>
2023-12-27 12:15:22 +0300

    fix: product like and order error fix

--- product/serializers.py
+++ product/serializers.py
@@ -15,16 +15,14 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def get_liked(self, obj):
         request = self.context.get('request')
-        user = request.user if request and hasattr(request, 'user') else None
-
-        if user and user.is_authenticated:
-            return LikeModel.objects.filter(user_id=user.id, pro_id=obj.id).exists()
+        user = str(request).split('/')[-2]
+        if user:
+            return LikeModel.objects.filter(user_id=user, pro_id=obj.id).exists()
         return False
 
     def get_ordered(self, obj):
         request = self.context.get('request')
-        user = request.user if request and hasattr(request, 'user') else None
-
-        if user and user.is_authenticated:
-            return OrderModel.objects.filter(user_id=user.id, product_id=obj.id).exists()
+        user = str(request).split('/')[-2]
+        if user:
+            return OrderModel.objects.filter(user=user, product=obj.id).exists()
         return False
